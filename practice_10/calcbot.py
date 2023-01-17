28afd33258624f4e278d6a913a3c5e72a5c1082b
kroilov <popsakiller@gmail.com>
2023-01-17 17:31:00 +0300

    practice 10

---
+++ practice_10/calcbot.py
@@ -0,0 +1,41 @@
+import telebot
+import datetime
+import numexpr
+
+
+def start(message):
+    bot.send_message(message.chat.id, 'Я калькубот! Что нужно посчитать!')
+
+
+def handle_text(message):
+    bot.send_message(message.chat.id, parse(message.text, message.chat.id))
+
+
+def log(message, chat_id):
+    with open("log.txt", "a") as logfile:
+        logfile.write(str(datetime.datetime.now()) + "\t" +
+                      str(chat_id) + "\t" + message + "\n")
+
+
+def calc(arg_str):
+    return str(numexpr.evaluate(arg_str))b
+
+
+def parse(text, chat_id):
+    try:
+        if text == "/help":
+            result = "В калькуляторе исопльзована библиотека numexpr"
+            log(text, chat_id)
+        else:
+            result = calc(text)
+            log(text, chat_id)
+    except:
+        result = "Ошибка! Повторите ввод: "
+        log("Error: \t" + text, chat_id)
+    return result
+
+
+bot = telebot.TeleBot(token="TOKEN")
+bot.message_handler(commands=["start"])(start)
+bot.message_handler(content_types=["text"])(handle_text)
+bot.polling(none_stop=True, interval=0)
